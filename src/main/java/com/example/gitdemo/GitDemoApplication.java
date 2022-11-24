a4c6d7044605b7cc2bb64aa5e5d37d1a0b0fdf1c
jeraldo <jeraldo_ma@sottop.cn>
2022-11-24 17:33:55 +0800

    hot-fix first commit

--- src/main/java/com/example/gitdemo/GitDemoApplication.java
+++ src/main/java/com/example/gitdemo/GitDemoApplication.java
@@ -9,6 +9,7 @@ public class GitDemoApplication {
     public static void main(String[] args) {
         System.out.println("jjjj");
         System.out.println("pppp");
+        System.out.println("hot-fix 分支");
         SpringApplication.run(GitDemoApplication.class, args);
     }
 
