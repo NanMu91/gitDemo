b9bc07039de6b0f9113243d19f1855183f389023
NanMu91 <118952428+NanMu91@users.noreply.github.com>
2022-11-25 09:28:13 +0800

    Update GitDemoApplication.java

--- src/main/java/com/example/gitdemo/GitDemoApplication.java
+++ src/main/java/com/example/gitdemo/GitDemoApplication.java
@@ -10,6 +10,7 @@ public class GitDemoApplication {
         System.out.println("pull一下");
         System.out.println("hot-fix1 分支");
         System.out.println("第一次push2");
+        System.out.println("github");
         SpringApplication.run(GitDemoApplication.class, args);
     }
 
