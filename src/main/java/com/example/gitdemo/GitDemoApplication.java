2e0d46193b01dfd31a179d1d23fa4d160e58fd02
jeraldo <jeraldo_ma@sottop.cn>
2022-11-25 08:55:39 +0800

    第一次push

--- src/main/java/com/example/gitdemo/GitDemoApplication.java
+++ src/main/java/com/example/gitdemo/GitDemoApplication.java
@@ -11,7 +11,7 @@ public class GitDemoApplication {
         System.out.println("pppp");
         System.out.println("master 分支1");
         System.out.println("hot-fix1 分支");
-        System.out.println("第一次push");
+        System.out.println("第一次push2");
         SpringApplication.run(GitDemoApplication.class, args);
     }
 
