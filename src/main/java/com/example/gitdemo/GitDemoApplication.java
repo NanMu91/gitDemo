832ba53935d8db0c692f156d9bc91a90bec0abbf
NanMu91 <118952428+NanMu91@users.noreply.github.com>
2022-11-25 08:59:46 +0800

    Update GitDemoApplication.java

--- src/main/java/com/example/gitdemo/GitDemoApplication.java
+++ src/main/java/com/example/gitdemo/GitDemoApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitDemoApplication {
 
     public static void main(String[] args) {
-        System.out.println("jjjj");
-        System.out.println("pppp");
-        System.out.println("master 分支1");
+        System.out.println("pull一下");
         System.out.println("hot-fix1 分支");
         System.out.println("第一次push2");
         SpringApplication.run(GitDemoApplication.class, args);
